98d7480c2e049e227a08e5d64e6bb2eee53b7997
Giselle Tamarozzi <giselletgomes@outlook.com.br>
2021-08-07 10:33:26 -0300

    Add project files.

---
+++ GitHubUsers/GitHubUsers/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GitHubUsers.Models
+{
+    public class User : ObservableObject
+    {
+        Guid id;
+        string login;
+        string password;
+
+        [Key]
+        public Guid Id
+        {
+            get => id;
+            set => SetProperty(ref id, value);
+        }
+
+        public string Login
+        {
+            get => login;
+            set => SetProperty(ref login, value);
+        }
+
+        public string Password
+        {
+            get => password;
+            set => SetProperty(ref password, value);
+        }
+
+        public User()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+
+}
